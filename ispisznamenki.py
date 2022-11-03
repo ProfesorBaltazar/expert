4733462646f6f80e9ce20020281ee88ffa4050b2
Tea Lužaić <tealuzaic@MacBook-Air-od-Tea.local>
2022-11-03 19:26:08 +0100

    Added znamenkebroja.py

---
+++ ispisznamenki.py
@@ -0,0 +1,5 @@
+n=int(input("Unesi broj:  "))
+while n!=0:
+    z = n%10
+    print(z)
+    n = n//10
---
+++ znamenkebroja.py
@@ -0,0 +1,5 @@
+n=int(input("Upiši broj:  "))
+while n !=0:
+    z = n%10
+    print(z)
+    n=n//10
