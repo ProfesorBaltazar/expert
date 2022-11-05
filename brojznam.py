1cbf404899851e046c30d6efaae02ac7c52ca87e
ProfesorBaltazar <davor.luzaic@gmail.com>
2022-11-05 14:51:00 +0100

    broj znamenki

--- brojznam.py
+++ brojznam.py
@@ -1,7 +1,7 @@
 n = int(input("Upiši cijeli broj:  "))
 br=0
-while n!=0:
-    br=br+1
-    n = n//10
+while n!=0:   #dok je n različito od nula ponavljaj
+    br=br+1   # brojač prolaska kroz petlju
+    n = n//10 # cjelobrojno dijeljenje, brisanje znamenke
 print(br)
  
