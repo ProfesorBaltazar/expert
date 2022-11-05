4d666382573f340b321144081aee970fd1e6d90f
ProfesorBaltazar <davor.luzaic@gmail.com>
2022-11-05 16:10:24 +0100

    zbroj znamenki

--- zbrojznam.py
+++ zbrojznam.py
@@ -1,8 +1,8 @@
 n=int(input("Unesite broj:  ")) # Unesite prirodan broj 
 zbroj=0            # postavite brojac na nulu
 while n!=0:        #ponavljaj sve dok je n razlicito od nula
-    z=n%10         # MOD izdvoji zadnju znamenku
-    zbroj=zbroj+z  # zbroji zadnju znamenku sa zbrojem
-    n=n//10        # cjelobrojno dijeljenje, obriši zadnju zanmenku
+    z=n%10         #MOD izdvoji zadnju znamenku
+    zbroj=zbroj+z  #zbroji zadnju znamenku sa zbrojem
+    n=n//10        #cjelobrojno dijeljenje, obriši zadnju zanmenku
 print(zbroj)
 
