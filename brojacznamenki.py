8651262f77b6a46250fa29b8cf1d457de3f5b702
ProfesorBaltazar <davor.luzaic@gmail.com>
2022-11-05 14:53:23 +0100

    broj pojedinih znamenki

--- brojacznamenki.py
+++ brojacznamenki.py
@@ -1,7 +1,7 @@
 n=int(input("Unesi broj:  "))
-brojac=0
+brojac=0 
 while n!=0:
-    if n%10==2:
+    if n%10==2:    # ako je znamenka jednaka 2 povećaj brojač za 1
         brojac=brojac+1
     n=n//10
 print(brojac)
