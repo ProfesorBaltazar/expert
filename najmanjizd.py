92796ba9e22ff2f1365a730afeac982c0688efcb
ProfesorBaltazar <davor.luzaic@gmail.com>
2022-11-06 15:20:35 +0100

    najmanji djeljitelj

--- najmanjizd.py
+++ najmanjizd.py
@@ -2,6 +2,6 @@ n=int(input("Unesi broj:  "))
 d=2  # d=2 jer je 1 najmanji djeljitelj
 while n%d!=0: # ponavljaj sve dok je ostatak pri dijeljenju različit od 0
     d=d+1
-print(n//d)
+print(d)
 
 
